088771da7db056501afdb69f89817f230d8eefaa
analanga <126185969+analanga@users.noreply.github.com>
2025-10-24 12:21:59 +0200

    Se pueden valorar los spots existentes con una valoración de 1 a 5 estrellas

---
+++ lib/features/interactive_map/data/models/rating_model.dart
@@ -0,0 +1,33 @@
+import 'package:uuid/uuid.dart';
+
+class Rating {
+  final int spotId; 
+  final String userId; 
+  final int value; 
+  final DateTime createdAt; 
+
+  Rating({
+    required this.spotId,
+    required this.userId,
+    required this.value,
+    required this.createdAt,
+  });
+
+  factory Rating.fromMap(Map<String, dynamic> map) {
+    return Rating(
+      spotId: map['id_spot'] as int,
+      userId: map['id_usuario'] as String,
+      value: map['puntuacion'] as int,
+      createdAt: DateTime.parse(map['fecha_valoracion'] as String),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id_spot': spotId,
+      'id_usuario': userId,
+      'puntuacion': value,
+      'fecha_valoracion': createdAt.toIso8601String(),
+    };
+  }
+}
